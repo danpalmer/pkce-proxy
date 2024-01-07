7ee96b87d29e75174ceeb5be415a8ce8bef623e4
Dan Palmer <dan@danpalmer.me>
2024-01-07 18:09:11 +1100

    Improve QoL for development, no HTTPS

--- index.ts
+++ index.ts
@@ -8,28 +8,36 @@ import index from "./src/routes/index";
 
 const PORT = parseInt(process.env.PORT || "5000");
 const HOST = process.env.HOST || "0.0.0.0";
+const PROD = process.env.NODE_ENV?.startsWith("prod") || false;
 
 const server = fastify();
 
-// redirect to https
 server.addHook("onRequest", (req, res, done) => {
-  if (req.headers["x-forwarded-proto"] !== "https") {
+  // If prod and not HTTPs, redirect and end.
+  if (PROD && req.headers["x-forwarded-proto"] !== "https") {
     res.redirect(302, "https://" + req.hostname + req.originalUrl);
-  } else {
+    return done();
+  }
+
+  // If prod, enable HSTS so that this only ever works on HTTPS.
+  if (PROD) {
     res.header(
       "Strict-Transport-Security",
       "max-age=31536000; includeSubDomains"
     );
-    res.header(
-      "Content-Security-Policy",
-      "default-src 'none'; base-uri 'none'; object-src 'none'; form-action 'none'; frame-ancestors 'none'; require-trusted-types-for 'script';"
-    );
-    res.header("X-Content-Type-Options", "nosniff");
-    res.header("X-Frame-Options", "DENY");
-    res.header("X-XSS-Protection", "1; mode=block");
-    res.header("Referrer-Policy", "no-referrer");
-    done();
   }
+
+  // Basic security headers to ensure we're not being misused
+  res.header(
+    "Content-Security-Policy",
+    "default-src 'none'; base-uri 'none'; object-src 'none'; form-action 'none'; frame-ancestors 'none'; require-trusted-types-for 'script';"
+  );
+  res.header("X-Content-Type-Options", "nosniff");
+  res.header("X-Frame-Options", "DENY");
+  res.header("X-XSS-Protection", "1; mode=block");
+  res.header("Referrer-Policy", "no-referrer");
+
+  return done();
 });
 
 server
