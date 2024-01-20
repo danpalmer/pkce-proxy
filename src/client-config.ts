3205eade743a2daafc8339e3f72fafe5a4df9046
Dan Palmer <dan@danpalmer.me>
2024-01-20 14:49:09 +1100

    Add some compression and the real fields

--- src/client-config.ts
+++ src/client-config.ts
@@ -1,22 +1,29 @@
 import { Buffer } from "buffer";
 
 import { decrypt, encrypt } from "./crypto";
+import { gunzipSync, gzipSync } from "zlib";
 
 export interface ConfigToken {
-  test?: string;
+  clientSecret: string;
+  authorizeUrl: string;
+  tokenUrl: string;
+  refreshTokenUrl?: string;
+  jsonOrForm?: "json" | "form";
 }
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
 export function decode(token: string): ConfigToken {
-  const buffer = Buffer.from(token, "base64");
+  const buffer = Buffer.from(decodeURIComponent(token), "base64");
   const decrypted = decrypt(buffer);
-  return JSON.parse(decoder.decode(decrypted)) as ConfigToken;
+  const decompressed = gunzipSync(decrypted);
+  return JSON.parse(decoder.decode(decompressed)) as ConfigToken;
 }
 
 export function encode(config: ConfigToken): string {
   const encoded = encoder.encode(JSON.stringify(config));
-  const encrypted = encrypt(encoded);
-  return Buffer.from(encrypted).toString("base64");
+  const compressed = gzipSync(encoded);
+  const encrypted = encrypt(compressed);
+  return encodeURIComponent(Buffer.from(encrypted).toString("base64"));
 }
