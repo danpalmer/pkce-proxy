f57697d40400a4ebcf93c097474e84ad0f12f07c
Dan Palmer <dan@danpalmer.me>
2024-03-31 21:18:48 +0100

    Switch to better ioredis

--- src/storage/redis.ts
+++ src/storage/redis.ts
@@ -1,55 +1,57 @@
-import * as redis from "redis";
+import Redis from "ioredis";
 
 import { Session, SessionStorage } from "./types";
-
-type RedisClient = redis.RedisClientType<
-  redis.RedisModules,
-  redis.RedisFunctions,
-  redis.RedisScripts
->;
+import { SESSION_MAX_SECONDS } from "../env";
 
 export default class RedisStorage implements SessionStorage {
-  client: Promise<RedisClient>;
+  client: Redis;
 
   constructor(redisUrl: string) {
-    this.client = redis.createClient({ url: redisUrl }).connect();
+    this.client = new Redis(redisUrl);
   }
 
   async get(state: string): Promise<Session | undefined> {
-    const client = await this.client;
-    const data = await client.get(`session:${state}`);
+    const data = await this.client.get(`session:${state}`);
     return (data && JSON.parse(data)) || undefined;
   }
 
   async getCode(code: string): Promise<string | undefined> {
-    const client = await this.client;
-    const state = await client.get(`code:${code}`);
+    const state = await this.client.get(`code:${code}`);
     return (state && state.toString()) || undefined;
   }
 
   async set(state: string, session: Session) {
-    const client = await this.client;
-    await client.set(`session:${state}`, JSON.stringify(session));
+    await this.client.set(
+      `session:${state}`,
+      JSON.stringify(session),
+      "EX",
+      SESSION_MAX_SECONDS
+    );
   }
 
   async setCode(code: string, session: Session) {
-    const client = await this.client;
-    await client.set(`code:${code}`, session.state);
-    await client.set(
+    await this.client.set(
+      `code:${code}`,
+      session.state,
+      "EX",
+      SESSION_MAX_SECONDS
+    );
+    await this.client.set(
       `session:${session.state}`,
-      JSON.stringify({ ...session, code: code })
+      JSON.stringify({ ...session, code: code }),
+      "EX",
+      SESSION_MAX_SECONDS
     );
   }
 
   async delete(state: string) {
-    const client = await this.client;
     const session = await this.get(state);
     if (!session) {
       return;
     }
-    await client.del(`session:${state}`);
+    await this.client.del(`session:${state}`);
     if (session.code) {
-      await client.del(`code:${session.code}`);
+      await this.client.del(`code:${session.code}`);
     }
   }
 }
